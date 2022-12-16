6824fc7f2d4ac35f3c4a36ad01dd493153462abf
HasanMdKamrul <kamrulhasan@iut-dhaka.edu>
2022-12-16 01:48:09 +0100

    gap removerd

--- components/Results.js
+++ components/Results.js
@@ -3,7 +3,7 @@ import Thumbnail from "./Thumbnail";
 const Results = ({ movies }) => {
   console.log(movies);
   return (
-    <div className="grid lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-4 grid-cols-1 gap-5 mx-5 mt-2">
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-4 grid-cols-1  mx-5 mt-2">
       {movies?.map((movie) => (
         <Thumbnail movie={movie} key={movie.id} />
       ))}
